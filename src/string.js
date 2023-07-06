110d199c4990f4f47949b66d1ccecfcf790c6fe2
Yesudeep Mangalapilly <yesudeep@google.com>
2023-07-06 08:24:11 -0700

    feat: some unit tests for some more utility functions.

--- src/string.js
+++ src/string.js
@@ -16,3 +16,27 @@ export function trimSpecialCharacters(
 ) {
   return str.replace(pattern, '$1$2');
 }
+
+/**
+ * Determines the label given a rule, a value, an existing label and an override.
+ *
+ * @param {string} rule The rule to match.
+ * @param {string} value The value to be used if the rule is mean to use a value.
+ * @param {string} label The label if the rule is meant to use a label.
+ * @param {string} override An override for the label.
+ * @return {string} The label based on the rule.
+ */
+export function getLabel(rule, value, label, override) {
+  label = override || label;
+  switch(rule) {
+    case 'value':
+      return `${value}`;
+    case 'label':
+      return `${label}`;
+    case 'both':
+      return `${value} ${label}`;
+    case 'none':
+    default:
+      return ``;
+  }
+}
