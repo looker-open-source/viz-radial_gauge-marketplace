acf8a65d3588d95b892030a763865e7c812b00ff
andres-rubio-go <rubioandres@google.com>
2023-11-16 18:27:17 +0000

    fix: Refactored error handling

--- src/radial_gauge_container.js
+++ src/radial_gauge_container.js
@@ -5,8 +5,11 @@ import {plotConfig} from './constants/plot_config';
 import {processPivot} from './functions/process_pivot';
 import {RadialGauge} from './radial_gauge';
 import {processData} from './functions/process_data';
+import {handleVizErrors} from './functions/viz_error_handler';
 
 const DEFAULT_MAX_RANGE = null;
+
+var radialGaugeViz;
 
 looker.plugins.visualizations.add({
   id: 'gauge',
@@ -24,48 +27,15 @@ looker.plugins.visualizations.add({
       width = element.clientWidth,
       height = element.clientHeight;
 
-    // Clear any errors from previous updates
-    this.clearErrors();
-
-    if (data.length < 1) {
-      // Issue identified where viz would not change with table calc filters
-      // need to supply the container with something new if we fail early and
-      // don't make it to the inteded render function.
-      // https://looker.atlassian.net/browse/DX-5779
-      this.chart = ReactDOM.render(<></>, this.container);
-      this.addError({
-        title: 'No Results',
-      });
-      return;
-    }
-
-    // Throw some errors and exit if the shape of the data isn't what this chart needs
-    if (
-      queryResponse.fields.dimension_like.length > 1 ||
-      queryResponse.fields.measure_like.length > 2
-    ) {
-      this.addError({
-        title: 'Invalid Input.',
-        message: 'This chart accepts up to 1 dimension and 2 measures.',
-      });
-      return;
-    }
-
-    if (
-      config.viz_trellis_by === 'pivot' &&
-      queryResponse.pivots === undefined
-    ) {
-      this.addError({
-        title: 'Invalid Input.',
-        message: 'Add pivots or change trellis type.',
-      });
-      return;
-    }
+    // Get a copy of the viz object
+    radialGaugeViz = this;
+    // Handle any erros with viz data and config
+    handleVizErrors(radialGaugeViz, data, queryResponse, config);
 
     // Catch case where config is undefined on db-next
-    for (let option in this.options) {
+    for (let option in radialGaugeViz.options) {
       if (config[option] === undefined) {
-        config[option] = this.options[option].default;
+        config[option] = radialGaugeViz.options[option].default;
       }
     }
 
@@ -78,7 +48,7 @@ looker.plugins.visualizations.add({
         data.length
       );
       data.forEach((d, i) => {
-        chunk = processData(data[i], queryResponse, config, this);
+        chunk = processData(data[i], queryResponse, config, radialGaugeViz);
         if (i <= limit - 1) {
           chunk_multiples.push(chunk);
         }
@@ -89,13 +59,19 @@ looker.plugins.visualizations.add({
         queryResponse.pivots.length
       );
       queryResponse.pivots.forEach((d, i) => {
-        chunk = processPivot(data, queryResponse, config, this, d.key);
+        chunk = processPivot(
+          data,
+          queryResponse,
+          config,
+          radialGaugeViz,
+          d.key
+        );
         if (i <= limit - 1) {
           chunk_multiples.push(chunk);
         }
       });
     } else {
-      chunk = processData(data, queryResponse, config, this);
+      chunk = processData(data, queryResponse, config, radialGaugeViz);
     }
 
     if (!config.range_max || config.range_max === DEFAULT_MAX_RANGE) {
@@ -108,9 +84,9 @@ looker.plugins.visualizations.add({
       let default_max = Math.ceil(num / fac) * fac;
       config.range_max = default_max;
     }
-    var viz = this;
+
     if (config.viz_trellis_by === 'none') {
-      viz.radialProps = {
+      radialGaugeViz.radialProps = {
         cleanup: `gauge`,
         trellis_by: config.viz_trellis_by,
         w: width,
@@ -124,8 +100,6 @@ looker.plugins.visualizations.add({
         gauge_background: config.background_color,
         range: [config.range_min, config.range_max],
         value: chunk.value > config.range_max ? config.range_max : chunk.value,
-        // value: config.dev_value,
-        // value_rendered: config.dev_value.toString(),
         value_rendered: chunk.value_rendered,
         target:
           chunk.target > config.range_max ? config.range_max : chunk.target,
@@ -166,9 +140,9 @@ looker.plugins.visualizations.add({
         wrap_width: 100,
       };
       // Finally update the state with our new data
-      viz.chart = ReactDOM.render(
-        <RadialGauge {...viz.radialProps} />,
-        viz.container
+      radialGaugeViz.chart = ReactDOM.render(
+        <RadialGauge {...radialGaugeViz.radialProps} />,
+        radialGaugeViz.container
       );
     } else {
       chunk_multiples.forEach(function (d, i) {
@@ -179,7 +153,7 @@ looker.plugins.visualizations.add({
                 config.trellis_cols * config.trellis_rows,
                 queryResponse.pivots.length
               );
-        viz.radialProps = {
+        radialGaugeViz.radialProps = {
           cleanup: `subgauge${i}`,
           trellis_by: config.viz_trellis_by,
           trellis_limit: limit,
@@ -232,9 +206,9 @@ looker.plugins.visualizations.add({
           target_label_padding: config.target_label_padding,
           wrap_width: 100,
         };
-        viz.chart = ReactDOM.render(
-          <RadialGauge {...viz.radialProps} />,
-          viz.container
+        radialGaugeViz.chart = ReactDOM.render(
+          <RadialGauge {...radialGaugeViz.radialProps} />,
+          radialGaugeViz.container
         );
       });
     }
